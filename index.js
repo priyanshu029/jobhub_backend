8688e237941877023fc51da5494f0a3afaca3d38
priyanshu <priyanshushekhar029@gmail.com>
2023-08-08 10:39:02 +0530

    changing_port_to_env_var

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 3000
-// const port = process.env.PORT;
+// const port = 3000
+const port = process.env.PORT;
 
 
 const dotenv = require('dotenv');
@@ -25,4 +25,4 @@ app.use("/api/bookmarks",bookmarkRoute)
 
 
 app.get('/', (req, res) => {res.send('Hello Priyanshu!')});
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port || 4000, () => console.log(`Example app listening on port ${port}!`))
