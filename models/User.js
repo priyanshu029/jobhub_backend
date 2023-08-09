e3f027427bd9c253f7ba8ef98fbaf38e34b90a6a
priyanshu <priyanshushekhar029@gmail.com>
2023-08-09 22:32:19 +0530

    addedPhoneToUserModel

--- models/User.js
+++ models/User.js
@@ -5,6 +5,7 @@ const UserSchema = new mongoose.Schema(
 
     username: {type:String, required:true, unique:true},
     email: {type:String, required:true, unique:true},
+    phone:{type:String,required:true,unique:true},
     password: {type: String, required:true},
     location: {type:String, required:false},
     isAdmin: {type:Boolean, default:false},
